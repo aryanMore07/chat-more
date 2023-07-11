7a49268384bb4bec7056da4c2496e42b2c738127
AryanMore <aryanmore498@gmail.com>
2023-07-11 21:04:01 +0530

    FEAT: Change the search model to drawer when in mobile size and change classname of landing page

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './navbar.css';
 import SearchComponent from '../searchComponent/SearchComponent';
+import { useLocation } from 'react-router';
 
 const Navbar = () => {
+
+  const location = useLocation();
+
+  const checkLocation = location.pathname === '/home' || location.pathname === '/explore' || location.pathname === '/bookmarks' || location.pathname === '/profile'
+  
   return (
     <div className='navbar-div'>
       <h2 className='heading'><span style={{ color: '#ff3b30' }}>Chat</span><span style={{ color: 'white' }}>More</span></h2>
-      <div className='search-div'>
+      <div className='search-div' style={{ display: checkLocation ? 'block' : 'none' }}>
         <SearchComponent />
       </div>
     </div>
--- src/component/searchComponent/SearchComponent.jsx
+++ src/component/searchComponent/SearchComponent.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './searchComponent.css';
 import Button from '@mui/material/Button';
 import SearchIcon from '@mui/icons-material/Search';
 import { useContext } from 'react';
 import { UserContext } from '../../contexts/userContext';
+import { styled } from "@mui/material/styles";
 import Modal from '@mui/material/Modal';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Box from '@mui/material/Box';
 // import { useNavigate } from 'react-router';
+import Drawer from '@mui/material/Drawer';
+import IconButton from "@mui/material/IconButton";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import { Avatar } from '@mui/material';
 
 const style = {
     position: 'absolute',
@@ -28,19 +34,70 @@ const style = {
     height: 'max-content'
 };
 
+const DrawerHeader = styled("div")(({ theme }) => ({
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end"
+}));
+
 const SearchComponent = () => {
 
     // const navigate = useNavigate();
     const { state, dispatch, searchedData } = useContext(UserContext);
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+    const [isopen, setIsOpen] = useState(false);
+    const handleIsOpen = () => setIsOpen(true);
+    const handleClose = () => setIsOpen(false);
 
     return (
         <div>
-            <Button variant='outlined' onClick={handleOpen}><SearchIcon /></Button>
-            <Modal
-                open={open}
+            <Button variant="outlined" onClick={handleIsOpen} style={{color: 'white', borderColor: 'white'}}><SearchIcon /></Button>
+            <Drawer anchor='right' open={isopen}>
+                <div style={{ padding: '2rem' }}>
+                    <DrawerHeader>
+                        <IconButton onClick={handleClose}>
+                            <ChevronRightIcon />
+                        </IconButton>
+                    </DrawerHeader>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon1"><SearchIcon /></InputGroup.Text>
+                        <Form.Control
+                            placeholder="Username"
+                            aria-label="Username"
+                            aria-describedby="basic-addon1"
+                            onChange={(event) => {
+                                dispatch({ type: 'USER_INPUT', payload: event.target.value });
+                            }}
+                        />
+                    </InputGroup>
+                </div>
+                <hr />
+                {searchedData.length === 0 && (<h3 style={{ textAlign: 'center' }}>Search peoples</h3>)}
+                {
+                    state.userInput &&
+                    (
+                        searchedData.map((user) => {
+                            const { _id, firstName, lastName, username, image } = user;
+                            return (
+                                <div key={_id} className='user-card'>
+                                    <div className='user-details'>
+                                        <ListItemButton>
+                                            <div style={{ padding: '15px' }}>
+                                                <Avatar src={image} alt={firstName} />
+                                            </div>
+                                            <ListItemText primary={firstName + lastName} secondary={`@${username}`} />
+                                        </ListItemButton>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    )
+                }
+            </Drawer>
+            {/* <Modal
+                Isopen={Isopen}
                 onClose={handleClose}
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
@@ -80,8 +137,8 @@ const SearchComponent = () => {
                         }
                     </div>
                 </Box>
-            </Modal>
-        </div>
+            </Modal> */}
+        </div >
     )
 }
 
