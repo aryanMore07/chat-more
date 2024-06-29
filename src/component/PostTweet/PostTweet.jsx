0bdcff4a41492a8178d561d748efd8c8680e8f3c
aryanMore07 <aryanmore498@gmail.com>
2024-06-29 12:26:25 +0530

    Fixing the tweet cards using mui styled components

--- src/component/PostTweet/PostTweet.jsx
+++ src/component/PostTweet/PostTweet.jsx
@@ -1,154 +1,107 @@
-import React, { useContext } from 'react';
-import './postTweet.css';
-import Avatar from '@mui/material/Avatar';
-import { UserContext } from '../../contexts/userContext';
+import React, { useContext } from "react";
+import "./postTweet.css";
+import Avatar from "@mui/material/Avatar";
+import { UserContext } from "../../contexts/userContext";
 import TextField from "@mui/material/TextField";
-import { PostContext } from '../../contexts/postContext';
-import { useState } from 'react';
-import FilterPostPopOver from '../filterPostPopOver/FilterPostPopOver';
-import { FilteredPostContext } from '../../contexts/filterPostContext';
-import { PostTime } from '../../utils/postTime/PostTime';
-import PostSettingComponent from '../postSettingComponent/PostSettingComponent';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { useLocation, useNavigate } from 'react-router';
+import { PostContext } from "../../contexts/postContext";
+import { useState } from "react";
+import FilterPostPopOver from "../filterPostPopOver/FilterPostPopOver";
+import { FilteredPostContext } from "../../contexts/filterPostContext";
+import { Grid } from "@mui/material";
+import TweetCard from "./card_widgets/TweetCard";
 
 const PostTweetComponent = () => {
-
-
-  let location = useLocation();
-  const navigate = useNavigate();
   const { state } = useContext(UserContext);
-  const { postMessage, bookmarkPost, postData, dislikePost, addToBookmark, removeFromBookmark, likePost, } = useContext(PostContext);
+  const { postMessage } = useContext(PostContext);
   const { filterPostData } = useContext(FilteredPostContext);
   const { firstName, lastName, username, image } = state.userDetails;
-  const bookmarkedPost = (postId) => bookmarkPost.find((post) => post._id === postId);
-  const likedPost = (postId) => postData.find(({ _id, likes }) => _id === postId && likes.likedBy.find(({ _id }) => _id === state.userDetails._id));
 
-  const [input, setInput] = useState({ content: "", firstName: "", lastName: "", username: "", image: "", comments: [] });
+  const [input, setInput] = useState({
+    content: "",
+    firstName: "",
+    lastName: "",
+    username: "",
+    image: "",
+    comments: [],
+  });
 
   const postBtnHandler = () => {
-    // console.log(input.content === '');
     if (input.content === "") {
-      alert('Please input the feild')
+      alert("Please input the feild");
     } else {
       postMessage(input);
-      setInput({ content: "", firstName: "", lastName: "", username: "", image: "", comments: [] });
+      setInput({
+        content: "",
+        firstName: "",
+        lastName: "",
+        username: "",
+        image: "",
+        comments: [],
+      });
     }
-  }
-
+  };
   return (
-    <div style={{ padding: '3rem' }}>
-      <h4 style={{ textAlign: 'center', marginBottom: '10px' }}><b>Home</b></h4>
-      <div className='post-something-div'>
-        <div className='post-user-profile-div'>
-          <Avatar alt={state.userDetails.firstName} src={state.userDetails.image} />
+    <div style={{ padding: "3rem" }}>
+      <h4 style={{ textAlign: "center", marginBottom: "10px" }}>
+        <b>Home</b>
+      </h4>
+      <div className="post-something-div">
+        <div className="post-user-profile-div">
+          <Avatar
+            alt={state.userDetails.firstName}
+            src={state.userDetails.image}
+          />
         </div>
-        <div style={{ width: '100%', padding: '18px' }}>
-          <div className='input-div'>
+        <div style={{ width: "100%", padding: "18px" }}>
+          <div className="input-div">
             <TextField
               id="filled-multiline-static"
               label="Whats happening...?"
               multiline
               rows={4}
               variant="filled"
-              className='post-input'
+              className="post-input"
               value={input.content}
               onChange={(event) => {
-                setInput({ content: event.target.value, firstName: firstName, lastName: lastName, username: username, image: image, comments: [] });
+                setInput({
+                  content: event.target.value,
+                  firstName: firstName,
+                  lastName: lastName,
+                  username: username,
+                  image: image,
+                  comments: [],
+                });
               }}
             />
           </div>
-          <div className='post-btn-div'>
-            <button className='post-btn' onClick={postBtnHandler}>Post</button>
+          <div className="post-btn-div">
+            <button className="post-btn" onClick={postBtnHandler}>
+              Post
+            </button>
           </div>
         </div>
       </div>
-      <div className='filter-div'>
+      <div className="filter-div">
         <FilterPostPopOver />
       </div>
-      <div className='user-posts'>
-              {
-                filterPostData.length === 0 && <h1>No posts to show!</h1>
-              }
-        {
-          filterPostData.map((post) => {
-            const { _id, username, content, likes, image, firstName, lastName, comments, createdAt } = post;
+      <div className="user-posts">
+        <Grid container spacing={2}>
+          {filterPostData.length === 0 && (
+            <Grid item md={12}>
+              <h1>No posts to show!</h1>
+            </Grid>
+          )}
+          {filterPostData.map((post, index) => {
             return (
-              <div key={_id} className='explore-post'>
-                <div className='user-img'>
-                  <Avatar alt={username} src={image} />
-                </div>
-                <div className='explore-post-details'>
-                  <div style={{
-                    display: 'flex',
-                    width: '100%',
-                    justifyContent: 'space-between'
-                  }}>
-                    <p onClick={() => {
-                      if (username === state?.userDetails?.username) {
-                        navigate('/profile', { state: { from: location } });
-                      } else {
-                        navigate(`/users/${username}`, { state: { from: location } });
-                      }
-                    }} className='user-tag' style={{ marginLeft: '15px' }}><b>{firstName}{lastName}</b> <span style={{ color: '#9a9a9a' }}>@{username} | {PostTime(createdAt)}</span></p>
-                    <div>
-                      <PostSettingComponent postId={_id} userName={username} />
-                    </div>
-                  </div>
-                  <div className='users-content'>
-                    <div onClick={() => {
-                      navigate(`/${_id}`, { state: { from: location } });
-                    }}>
-                      {content}
-                    </div>
-                    <hr />
-                    <div className='icons-div'>
-                      {
-                        likedPost(_id) ?
-                          (
-                            <span className='icons' onClick={() => {
-                              dislikePost(_id)
-                            }}><FavoriteIcon /> {likes.likeCount}</span>
-                          )
-                          :
-                          (
-                            <span className='icons' onClick={() => {
-                              likePost(_id)
-                            }}><FavoriteBorderIcon /> {likes.likeCount}</span>
-                          )
-                      }
-                      <span className='icons' onClick={() => {
-                        navigate(`/${_id}`, { state: { from: location } });
-                        // <Navigate to={`/${_id}`} state={{ from: location }} />
-                      }}><ChatBubbleOutlineIcon /> {comments.length}</span>
-                      {
-                        bookmarkedPost(_id) ?
-                          (
-                            <span className='icons' onClick={() => {
-                              removeFromBookmark(_id)
-                            }}><BookmarkIcon /></span>
-                          )
-                          :
-                          (
-                            <span className='icons' onClick={() => {
-                              addToBookmark(_id)
-                            }} ><BookmarkBorderIcon /></span>
-                          )
-                      }
-                    </div>
-                  </div>
-                </div>
-              </div>
-            )
-          })
-        }
+              <Grid item md={12} key={index}>
+                <TweetCard data={post} />
+              </Grid>
+            );
+          })}
+        </Grid>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default PostTweetComponent;
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -15,6 +15,22 @@ import ListItemText from "@mui/material/ListItemText";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import LogoutComponent from "../../component/logoutComponent/LogoutComponent";
 import LogoutIcon from "@mui/icons-material/Logout";
+import { styled } from "@mui/material/styles";
+import { Box, Grid } from "@mui/material";
+
+const Container = styled(Box)(({ theme }) => ({
+  width: "90%",
+  margin: "auto",
+  height: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+}));
+
+const MiddleContainer = styled(Box)(({ theme }) => ({
+  width: "100%",
+  minHeight: "calc(100vh - 65px)",
+}));
 
 const HomeComponent = () => {
   const navigate = useNavigate();
@@ -22,164 +38,172 @@ const HomeComponent = () => {
     useContext(UserContext);
 
   return (
-    <div className="home-div container layout">
-      <div className="leftSide-div aside-left">
-        <ul className="leftSide-ulelements" style={{}}>
-          <NavLink to="/home" className="sidebar-nav-links">
-            <li className="list-style">
-              <span className="action-icons">
-                <HomeIcon />
-              </span>
-              <span className="route-names">Home</span>
-            </li>
-          </NavLink>
-          <NavLink to="explore" className="sidebar-nav-links">
-            <li className="list-style">
-              <span className="action-icons">
-                <RocketLaunchIcon />
-              </span>
-              <span className="route-names">Explore</span>
-            </li>
-          </NavLink>
-          <NavLink to="bookmarks" className="sidebar-nav-links">
-            <li className="list-style">
-              <span className="action-icons">
-                <BookmarkBorderIcon />
-              </span>
-              <span className="route-names">Bookmark</span>
-            </li>
-          </NavLink>
-          <NavLink to="profile" className="sidebar-nav-links">
-            <li className="list-style">
-              <span className="action-icons">
-                <AccountCircleIcon />
-              </span>
-              <span className="route-names">Profile</span>
-            </li>
-          </NavLink>
-          <NavLink
-            className="sidebar-nav-links logout-btn"
-            style={{ color: "black" }}
-            onClick={() => {
-              dispatch({ type: "LOGOUT_USER" });
-            }}
-          >
-            <li className="list-style">
-              <span className="logout-btn action-icons">
-                <LogoutIcon />
-              </span>
-            </li>
-          </NavLink>
-        </ul>
-        {state?.userDetails?.firstName && (
-          <div className="user-div">
-            <div>
-              <Avatar alt="Travis Howard" src={state?.userDetails?.image} />
-            </div>
-            <div>
-              <p style={{ margin: "0px" }}>
-                <b>
-                  {state.userDetails.firstName}
-                  {state.userDetails.lastName}
-                </b>
-              </p>
-              <p style={{ margin: "0px" }}>@{state.userDetails.username}</p>
-            </div>
-            <div className="user-menu-btn">
-              <LogoutComponent />
-            </div>
+    <Container>
+      <Grid container>
+        <Grid item md={2}>
+          <div className="leftSide-div aside-left">
+            <ul className="leftSide-ulelements" style={{}}>
+              <NavLink to="/home" className="sidebar-nav-links">
+                <li className="list-style">
+                  <span className="action-icons">
+                    <HomeIcon />
+                  </span>
+                  <span className="route-names">Home</span>
+                </li>
+              </NavLink>
+              <NavLink to="explore" className="sidebar-nav-links">
+                <li className="list-style">
+                  <span className="action-icons">
+                    <RocketLaunchIcon />
+                  </span>
+                  <span className="route-names">Explore</span>
+                </li>
+              </NavLink>
+              <NavLink to="bookmarks" className="sidebar-nav-links">
+                <li className="list-style">
+                  <span className="action-icons">
+                    <BookmarkBorderIcon />
+                  </span>
+                  <span className="route-names">Bookmark</span>
+                </li>
+              </NavLink>
+              <NavLink to="profile" className="sidebar-nav-links">
+                <li className="list-style">
+                  <span className="action-icons">
+                    <AccountCircleIcon />
+                  </span>
+                  <span className="route-names">Profile</span>
+                </li>
+              </NavLink>
+              <NavLink
+                className="sidebar-nav-links logout-btn"
+                style={{ color: "black" }}
+                onClick={() => {
+                  dispatch({ type: "LOGOUT_USER" });
+                }}
+              >
+                <li className="list-style">
+                  <span className="logout-btn action-icons">
+                    <LogoutIcon />
+                  </span>
+                </li>
+              </NavLink>
+            </ul>
+            {state?.userDetails?.firstName && (
+              <div className="user-div">
+                <div>
+                  <Avatar alt="Travis Howard" src={state?.userDetails?.image} />
+                </div>
+                <div>
+                  <p style={{ margin: "0px" }}>
+                    <b>
+                      {state.userDetails.firstName}
+                      {state.userDetails.lastName}
+                    </b>
+                  </p>
+                  <p style={{ margin: "0px" }}>@{state.userDetails.username}</p>
+                </div>
+                <div className="user-menu-btn">
+                  <LogoutComponent />
+                </div>
+              </div>
+            )}
           </div>
-        )}
-      </div>
-      <div className="middle-div middle">
-        <Outlet />
-      </div>
-      <div className="rightSide-div aside-right">
-        <input
-          placeholder="Search Username..."
-          aria-label="Username"
-          aria-describedby="basic-addon1"
-          onChange={(event) => {
-            dispatch({ type: "USER_INPUT", payload: event.target.value });
-          }}
-        />
-        {state.userInput && (
-          <div className="searched-users-output">
-            <ul
-              style={{
-                listStyle: "none",
-                display: "flex",
-                flexDirection: "column",
+        </Grid>
+        <Grid item md={8}>
+          <MiddleContainer>
+            <Outlet />
+          </MiddleContainer>
+        </Grid>
+        <Grid item md={2}>
+          <div className="rightSide-div aside-right">
+            <input
+              placeholder="Search Username..."
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+              onChange={(event) => {
+                dispatch({ type: "USER_INPUT", payload: event.target.value });
               }}
-            >
-              {searchedData.map((user) => {
-                const { _id, firstName, lastName, username } = user;
+            />
+            {state.userInput && (
+              <div className="searched-users-output">
+                <ul
+                  style={{
+                    listStyle: "none",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  {searchedData.map((user) => {
+                    const { _id, firstName, lastName, username } = user;
+                    return (
+                      <li
+                        style={{
+                          padding: "0.5rem 1rem",
+                          border: "1px solid",
+                          borderColor: "rgba(0,0,0,0.2)",
+                          margin: "0.5rem",
+                          borderRadius: "0.5rem",
+                          boxShadow: "0 2px 8px 0 rgba(0,0,0,0.1)",
+                          cursor: "pointer",
+                        }}
+                        key={_id}
+                        onClick={() => {
+                          navigate(`/user/${_id}`);
+                        }}
+                      >
+                        <div
+                          style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            width: "100%",
+                          }}
+                        >
+                          <Avatar alt={firstName} />
+                          <div>
+                            <b>
+                              {firstName} {lastName}
+                            </b>{" "}
+                            <br />@{username}
+                          </div>
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            )}
+            <div className="all-user">
+              {allUserData.map((user) => {
+                const { _id, firstName, lastName, username, image } = user;
                 return (
-                  <li
-                    style={{
-                      padding: "0.5rem 1rem",
-                      border: "1px solid",
-                      borderColor: "rgba(0,0,0,0.2)",
-                      margin: "0.5rem",
-                      borderRadius: "0.5rem",
-                      boxShadow: "0 2px 8px 0 rgba(0,0,0,0.1)",
-                      cursor: "pointer",
-                    }}
-                    key={_id}
-                    onClick={() => {
-                      navigate(`/user/${_id}`);
-                    }}
-                  >
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        width: "100%",
-                      }}
-                    >
-                      <Avatar alt={firstName} />
-                      <div>
-                        <b>
-                          {firstName} {lastName}
-                        </b>{" "}
-                        <br />@{username}
-                      </div>
-                    </div>
-                  </li>
+                  <List key={_id}>
+                    <ListItem style={{ borderRadius: "5rem" }} disablePadding>
+                      <ListItemButton
+                        onClick={() => {
+                          navigate(`/user/${_id}`);
+                        }}
+                      >
+                        <ListItemIcon>
+                          <Avatar alt={firstName} src={image} />
+                        </ListItemIcon>
+                        <ListItemText
+                          primary={`${firstName}${lastName}`}
+                          secondary={"@" + username}
+                        />
+                        <ListItemIcon>
+                          <AddCircleOutlineIcon />
+                        </ListItemIcon>
+                      </ListItemButton>
+                    </ListItem>
+                  </List>
                 );
               })}
-            </ul>
+            </div>
           </div>
-        )}
-        <div className="all-user">
-          {allUserData.map((user) => {
-            const { _id, firstName, lastName, username, image } = user;
-            return (
-              <List key={_id}>
-                <ListItem style={{ borderRadius: "5rem" }} disablePadding>
-                  <ListItemButton
-                    onClick={() => {
-                      navigate(`/user/${_id}`);
-                    }}
-                  >
-                    <ListItemIcon>
-                      <Avatar alt={firstName} src={image} />
-                    </ListItemIcon>
-                    <ListItemText
-                      primary={`${firstName}${lastName}`}
-                      secondary={"@" + username}
-                    />
-                    <ListItemIcon>
-                      <AddCircleOutlineIcon />
-                    </ListItemIcon>
-                  </ListItemButton>
-                </ListItem>
-              </List>
-            );
-          })}
-        </div>
-      </div>
-    </div>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
